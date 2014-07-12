d9017fd604c740d8b0408a4fdce7ea28ea57a94a
David Flicker <dflicker@caltech.edu>
2014-07-11 21:35:00 -0700

    added project dependecies

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ring "1.3.0"]
+                 [compojure "1.1.8"]
+                 [korma "0.3.2"]
+                 [hiccup "1.0.5"]]
   :main ^:skip-aot fantasy-rotation.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
